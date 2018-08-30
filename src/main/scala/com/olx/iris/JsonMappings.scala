754d420cf3007755db14acb21e0b4ca5a9d4d721
Adam Robinson <adam.robinson@olx.com>
2018-08-30 13:09:23 +0200

    fix: + added ZonedDateTime Json format via stackoverflow + gave up on Enumeration and sealed class serilization for present

--- src/main/scala/com/olx/iris/JsonMappings.scala
+++ src/main/scala/com/olx/iris/JsonMappings.scala
@@ -1,29 +1,12 @@
 package com.olx.iris
 
 import java.time.ZonedDateTime
-
-import com.olx.iris.CustomerType.CustomerType
-import com.olx.iris.ProductType.ProductType
-import spray.json.DefaultJsonProtocol
-
-object CustomerType extends Enumeration {
-  type CustomerType = Value
-  val BUSINESS, RESIDENTIAL = Value
-}
-
-object ProductType extends Enumeration {
-  type ProductType = Value
-  val AD_UPGRADE = Value("CashPurchase")
-  val EFFORT = Value("Reward")
-  val LIMITS = Value("CashPurchase")
-  val REWARD = Value("Reward")
-  val TOPUP = Value("WalletTopUp")
-}
+import spray.json.{DefaultJsonProtocol, DeserializationException, JsString, JsValue, RootJsonFormat}
 
 case class Customer(
   address: Address,
   businessName: Option[String],
-  customerType: CustomerType,
+  customerType: String,
   emailAddress: String,
   firstName: String,
   language: String,
@@ -56,14 +39,14 @@ case class MonetaryAmount(
   amount: BigDecimal
 )
 
-case class Product(
+case class Item(
   currency: String = "CRD",
   description: String,
   discountAmount: Option[BigDecimal],
   expiryDateTime: Option[ZonedDateTime],
   grossPrice: BigDecimal = BigDecimal(0.00),
   netPrice: Option[BigDecimal],
-  productType: ProductType,
+  productType: String,
   unitPrice: BigDecimal = BigDecimal(1.00),
   units: BigInt,
   vatAmount: Option[BigDecimal],
@@ -71,20 +54,30 @@ case class Product(
 )
 
 case class Transaction(
-  val customer: Customer,
-  val paymentReference: PaymentReference,
-  val products: List[Product],
-  val transactionReference: String
+  customer: Customer,
+  paymentReference: PaymentReference,
+  items: List[Item],
+  transactionReference: String
 )
 
+
 trait JsonMappings extends DefaultJsonProtocol {
+
+  implicit object ZonedDateTimeJsonFormat extends RootJsonFormat[ZonedDateTime] {
+
+    override def write(obj: ZonedDateTime) = JsString(obj.toString)
+
+    override def read(json: JsValue): ZonedDateTime = json match {
+      case JsString(s) => ZonedDateTime.parse(s)
+      case _ => throw new DeserializationException("Error info you want here ...")
+    }
+  }
+
   implicit val addressFormat = jsonFormat6(Address)
   implicit val amountFormat = jsonFormat2(MonetaryAmount)
-  implicit val customerTypeFormat = jsonFormat1(CustomerType)
-  implicit val productTypeFormat = jsonFormat1(ProductType)
   implicit val vatDataFormat = jsonFormat2(VatData)
   implicit val customerFormat = jsonFormat9(Customer)
   implicit val paymentReferenceFormat = jsonFormat2(PaymentReference)
-  implicit val productFormat = jsonFormat11(Product)
+  implicit val productFormat = jsonFormat11(Item)
   implicit val transactionFormat = jsonFormat4(Transaction)
 }
