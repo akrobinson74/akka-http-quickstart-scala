51843b429674b0cf64f26bf812b793e641b6b77e
Adam Robinson <akrobinson74@gmail.com>
2018-09-10 00:26:58 +0200

    starting on route def

--- src/main/scala/com/olx/iris/IrisService.scala
+++ src/main/scala/com/olx/iris/IrisService.scala
@@ -2,6 +2,7 @@ package com.olx.iris
 
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
+import com.olx.iris.model.Transaction
 import com.olx.iris.mongodb.Mongo
 
 trait Iris extends Mongo {
@@ -9,7 +10,7 @@ trait Iris extends Mongo {
     pathPrefix("orders") {
       path(Segment) { transactionReference =>
         get {
-          entity(as []) {
+          entity(as [Transaction]) {
 
           }
         }
