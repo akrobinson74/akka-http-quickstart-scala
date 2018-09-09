72899f664094796d319a48e47df57890797d05e1
Adam Robinson <adam.robinson@olx.com>
2018-09-09 17:27:19 +0200

    ok, further w/Mongo implementation :)

--- src/main/scala/com/olx/iris/TransactionActor.scala
+++ src/main/scala/com/olx/iris/TransactionActor.scala
@@ -1,9 +1,11 @@
 package com.olx.iris
 
 import akka.actor.{Actor, ActorLogging, Props}
-import com.olx.iris.TransactionActor.CreateTransaction
+import com.olx.iris.TransactionActor.{CreateTransaction, GetTransactionById}
 import com.olx.iris.model.{Integrator, Transaction}
+import com.olx.iris.mongodb.Mongo
 import org.mongodb.scala.MongoDatabase
+import org.mongodb.scala.bson.ObjectId
 
 object TransactionActor {
   final case class CreateTransaction(transaction: Transaction, integrator: Integrator)
@@ -15,8 +17,18 @@ object TransactionActor {
 
 class TransactionActor(db: MongoDatabase) extends Actor with ActorLogging {
 
+  val TX_COLLECTION: String = "transactions"
+
+  def createTransaction(db: MongoDatabase, tx: Transaction, int: Integrator)=
+    Mongo.transactionCollection.insertOne(tx)
+
+  def getTransaction(txId: String, int: Integrator) =
+    Mongo.transactionCollection.find(txId)()
+
   override def receive: Receive = {
-    case CreateTransaction =>
-//      sender() !
+    case CreateTransaction(tx: Transaction, int: Integrator) =>
+      sender() ! createTransaction(db, tx, int)
+    case GetTransactionById(txId: String, int: Integrator) =>
+      sender() ! getTransaction(txId, int)
   }
 }
